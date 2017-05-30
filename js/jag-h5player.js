f2a942a5cc7b8b3cf0bc52ac0f48a896f7685215
Henry Zhang <zzzsgsb@gmail.com>
2017-05-30 04:48:14 -0400

    xx

--- js/jag-h5player.js
+++ js/jag-h5player.js
@@ -1,5 +1,9 @@
 /* Get the jQuery object from the dom tree. */
-var video = $('#jagVideo');
+var video = $('#jag-video');
+var playButton = $('#play-button');
+var volumeButton = $('#volume-button');
+var progressBar = $('#jag-progress-bar');
+var bufferBar = $('#jag-buffer-bar');
 
 /* Listening to the keyboard event (keydown here). */
 $(document).on('keydown', function(event) {
@@ -57,7 +61,8 @@ video.on('click', function() {
     }
 });
 
-/* Listening to mouse double click event. */
+/* Listening to mouse double click event.
+*  监听鼠标双击事件。 */
 video.on('dblclick', function() {
     /* Check if the video is alrealy in fullscreen mode. */
     if (document.fullscreenElement || document.webkitFullscreenElement ||
@@ -86,8 +91,118 @@ video.on('dblclick', function() {
     }
 });
 
+/* 动态生成控制栏。 */
+function rebuildControlPanel() {
+    /* 获取当前视频高和宽。 */
+    var videoHeight = video.height() - 35;
+    var videoWidth = video.width();
+    /* 获取视频左偏移量。 */
+    leftOffset = video.offsetParent()[0].offsetLeft;
+    /* 控制栏高度为40px。 */
+    $('#control-panel').height(35).css({
+        /* 绝对定位确定控制栏位置。 */
+        'position': 'absolute',
+        'top': videoHeight,
+        'left': leftOffset,
+        'width': videoWidth,
+        //'background': 'black',
+        'display': 'block'
+    });
+}
+/* 初始化控制栏。 */
+rebuildControlPanel();
+
+/* 监听浏览器窗口大小改变时控制栏改变大小。 */
+$(window).on('resize', rebuildControlPanel);
+
+/* 监听全屏事件，退出时修正控制栏位置。 */
+$(document).on('fullscreenchange', function () {
+    rebuildControlPanel();
+});
+$(document).on('mozfullscreenchange', function () {
+    rebuildControlPanel();
+});
+$(document).on('webkitfullscreenchange', function () {
+    rebuildControlPanel();
+});
+$(document).on("msfullscreenchange", function () {
+    rebuildControlPanel();
+});
+
+/* 暂停时不管鼠标在哪里都不取消控制栏。开始时，移动到视频区域内显示，到外面立即取消，在里面不动5s取消。 */
 
 
+
+/* 根据当前时间更新进度条。 */
+video.on('timeupdate', function() {
+    var maxDuration = video[0].duration;
+    /* 获取当前时间。 */
+    var currentPos = video[0].currentTime; //Get currenttime
+    /* 获取进度条百分比。 */
+    var percentage = 100 * currentPos / maxDuration; //in %
+    /* 修改css中width属性改变进度条。 */
+    progressBar.css('width', percentage + '%');
+});
+
+//loop to get HTML5 video buffered data
+function startBuffer() {
+    var maxDuration = video[0].duration;
+    /* 获取视频当前缓冲进度。 */
+    var currentBuffer = video[0].buffered.end(0);
+    var percentage = 100 * (currentBuffer - video[0].currentTime) / maxDuration;
+    bufferBar.css('width', percentage + '%');
+    /* 只要未完全缓冲，就每500ms更新一次缓冲条。 */
+    if(currentBuffer < maxDuration) {
+        setTimeout(startBuffer, 500);
+    } else {
+        bufferBar.css('width', '100%');
+    }
+};
+/* 开始第一次更新缓冲条。  */
+setTimeout(startBuffer, 500);
+
+/* 点击播放按钮时，开始或暂停视频。 */
+playButton.on('click', function() {
+    if (video[0].paused) {
+        video[0].play();
+    } else {
+        video[0].pause();
+    }
+});
+
+volumeButton.on('click', function() {
+    video[0].muted = !video[0].muted;
+});
+
+video.on('pause', function() {
+    /* 当视频暂停时，修改按钮样式。 */
+    playButton.removeClass();
+    playButton.addClass('glyphicon glyphicon-play');
+});
+
+video.on('play', function() {
+    /* 当视频开始时，修改按钮样式。 */
+    playButton.removeClass();
+    playButton.addClass('glyphicon glyphicon-pause');
+});
+
+video.on('volumechange', function() {
+    /* 当音量变化时，修改音量键图标。 */
+    if (video[0].muted) {
+        volumeButton.removeClass();
+        volumeButton.addClass('glyphicon glyphicon-volume-off');
+    } else {
+        volumeButton.removeClass();
+        volumeButton.addClass('glyphicon glyphicon-volume-up');
+    }
+});
+
+
+
+
+
+/* Set context menu for right click. */
+/* 设置视频右键菜单。 */
 video.contextmenu({
     target: '#context-menu',
     before: function (e) {
@@ -99,7 +214,7 @@ video.contextmenu({
             this.closemenu();
             return false;
         }
-        this.getMenu().find("li").eq(2).find('a').html("This was dynamically changed");
+        //this.getMenu().find("li").eq(2).find('a').html("This was dynamically changed");
         return true;
     }
 });
@@ -107,30 +222,13 @@ video.contextmenu({
 
 
 
-/* .play() and .pause() method. */
-$('.btnPlay').on('click', function() {
-    if(video[0].paused) {
-        video[0].play();
-    }
-    else {
-        video[0].pause();
-    }
-    return false;
-});
-
 
 /* Get duration from loadedmettadata. */
 video.on('loadedmetadata', function() {
     $('.duration').text(video[0].duration);
 });
 
-/* Get currentTime from timeupdate. */
-video.on('timeupdate', function() {
-    var currentPos = video[0].currentTime; //Get currenttime
-    var maxduration = video[0].duration; //Get video duration
-    var percentage = 100 * currentPos / maxduration; //in %
-    $('.timeBar').css('width', percentage+'%');
-});
+
 
 var timeDrag = false;   /* Drag status */
 $('.progressBar').mousedown(function(e) {
@@ -180,13 +278,7 @@ var startBuffer = function() {
         setTimeout(startBuffer, 500);
     }
 };
-setTimeout(startBuffer, 500);
-
-//Mute/Unmute control clicked
-$('.muted').click(function() {
-    video[0].muted = !video[0].muted;
-    return false;
-});
+//setTimeout(startBuffer, 500);
 
 //Volume control clicked
 $('.volumeBar').on('mousedown', function(e) {
@@ -201,27 +293,5 @@ $('.ff').on('click', function() {
     video[0].pause();
     video[0].playbackRate = 2;
     video[0].play();
-    return false;
-});
-
-//Rewind control
-$('.rw').on('click', function() {
-    video[0].playbackrate = -3;
-    return false;
-});
-
-//Slow motion control
-$('.sl').on('click', function() {
-    video[0].playbackrate = 0.5;
-    return false;
-});
-
-$('.fullscreen').on('click', function() {
-    //For Webkit
-    video[0].webkitEnterFullscreen();
-
-    //For Firefox
-    video[0].mozRequestFullScreen();
-
     return false;
 });
