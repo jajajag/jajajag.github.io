813ccf42fea666ff0eb600cb238d26cbd728f34a
Henry Zhang <zzzsgsb@gmail.com>
2017-06-01 03:20:06 -0400

    update

--- js/jag-h5player.js
+++ js/jag-h5player.js
@@ -1,5 +1,6 @@
 /* Get the jQuery object from the dom tree. */
 var video = $('#jag-video');
+var fullscreenItem = $('#fullscreen-item');
 
 function playOrPause() {
     /* 暂停时播放，否则暂停。 */
@@ -10,6 +11,7 @@ function playOrPause() {
     }
 }
 
+/* 1. 键盘事件 */
 /* Listening to the keyboard event (keydown here). */
 $(document).on('keydown', function(event) {
     switch (event.which) {
@@ -34,6 +36,7 @@ $(document).on('keydown', function(event) {
     }
 });
 
+/* 2. 鼠标点击视频事件 */
 /* There should be an interval between two single click.
 *  We overlook the single click if two clicks are too close.
 *  singleClickFlag is a lock of clicks. */
@@ -74,14 +77,14 @@ function getFullscreen() {
         }
     } else {
         /* Request fullscreen if corresponding element exists. */
-        if (video[0].requestFullscreen) {
-            video[0].requestFullscreen();
-        } else if (video[0].webkitRequestFullscreen) {
-            video[0].webkitRequestFullscreen();
-        } else if (video[0].mozRequestFullScreen) {
-            video[0].mozRequestFullScreen();
-        } else if (video[0].msRequestFullscreen) {
-            video[0].msRequestFullscreen();
+        if (fullscreenItem[0].requestFullscreen) {
+            fullscreenItem[0].requestFullscreen();
+        } else if (fullscreenItem[0].webkitRequestFullscreen) {
+            fullscreenItem[0].webkitRequestFullscreen();
+        } else if (fullscreenItem[0].mozRequestFullScreen) {
+            fullscreenItem[0].mozRequestFullScreen();
+        } else if (fullscreenItem[0].msRequestFullscreen) {
+            fullscreenItem[0].msRequestFullscreen();
         }
     }
 }
@@ -90,22 +93,18 @@ function getFullscreen() {
 *  监听鼠标双击事件。 */
 video.on('dblclick', getFullscreen);
 
+
+/* 3. 控制栏 */
 var controlPanel = $('#control-panel');
+var progressButton = $('#progress-button');
 
 /* 动态生成控制栏。 */
 function rebuildControlPanel() {
-    /* 获取当前视频高和宽。 */
-    var videoHeight = video.height() - 40;
-    var videoWidth = video.width();
-    /* 获取视频左偏移量。 */
-    leftOffset = video.offsetParent()[0].offsetLeft;
-    /* 控制栏高度为40px。 */
-    controlPanel.height(40).css({
-        /* 绝对定位确定控制栏位置。 */
-        'position': 'absolute',
-        'top': videoHeight,
-        'left': leftOffset,
-        'width': videoWidth,
+    /* 控制栏高度为50px。 */
+    controlPanel.css({
+        /* 获取当前视频高和宽。 */
+        'top': video.height() - 50,
+        'width': video.width()
     });
 }
 /* 初始化控制栏。 */
@@ -114,7 +113,7 @@ rebuildControlPanel();
 /* 监听浏览器窗口大小改变时控制栏改变大小。 */
 $(window).on('resize', rebuildControlPanel);
 
-/* 监听全屏事件，退出时修正控制栏位置。 */
+/* 监听全屏事件，进入或退出时修正控制栏位置。 */
 $(document).on('fullscreenchange', function () {
     rebuildControlPanel();
 });
@@ -128,14 +127,11 @@ $(document).on("msfullscreenchange", function () {
     rebuildControlPanel();
 });
 
-/* 暂停时不管鼠标在哪里都不取消控制栏。开始时，移动到视频区域内显示，到外面立即取消，在里面不动5s取消。 */
-
-
-
+/* 4. 进度条 */
 var progressBar = $('#jag-progress-bar');
 var bufferBar = $('#jag-buffer-bar');
 
-/* 根据当前时间更新进度条和缓冲条。 */
+/* 根据当前时间更新进度条，缓冲条和按钮。 */
 video.on('timeupdate', function() {
     var maxDuration = video[0].duration;
     /* 获取当前时间和缓冲条。 */
@@ -144,8 +140,15 @@ video.on('timeupdate', function() {
     /* 获取进度条和缓冲条百分比。 */
     var perCurrent = 100 * currentPos / maxDuration;
     var perBuffer = 100 * (currentBuffer - currentPos) / maxDuration;
+    /* 进度条长度为视频长度减去边缘长度。 */
+    var progressWidth = video.width() - 30;
     /* 修改css中width属性改变进度条。 */
     progressBar.css('width', perCurrent + '%');
+    /* 修改css中left属性改变进度按钮。 */
+    progressButton.css({
+        /* 按钮的左偏移量为7，根据当前时间更新按钮。 */
+        left: 7 + progressWidth * currentPos / maxDuration
+    });
     /* 未完全缓冲时。 */
     if (currentBuffer < maxDuration) {
         bufferBar.css('width', perBuffer + '%');
@@ -153,6 +156,23 @@ video.on('timeupdate', function() {
         bufferBar.css('width', (100 - perCurrent) + '%');
     }
 });
+
+/* 进度条点击事件。 */
+$('.progress-bar-container').on('click', function(event) {
+    /* 点击事件的偏移量为相对文档的偏移量减去视频偏移量减去控制栏。 */
+    var leftOffset = event.pageX - video.offset().left - 15;
+    /* 获取控制栏总长度。 */
+    var progressWidth = video.width() - 30;
+    /* 点击坐标的左偏移量除以控制栏长度，和播放时间百分比相等。 */
+    video[0].currentTime = leftOffset / progressWidth * video[0].duration;
+});
+
+/* 进度按钮拖动事件。 */
+
+
+
+
+
 
 /* 在播放前调用此函数更新缓冲条。 */
 function startBuffer() {
@@ -171,6 +191,7 @@ function startBuffer() {
 /* 开始第一次更新缓冲条。 */
 setTimeout(startBuffer, 1000);
 
+/* 5. 控制栏按钮 */
 var playButton = $('#play-button');
 var volumeButton = $('#volume-button');
 var fullscreenButton = $('#fullscreen-button');
@@ -207,13 +228,14 @@ $('.list-group-item').on('click', function() {
     setTimeout(function() {playbackList.hide('fast');}, 300);
 });
 
+/* 6. 视频本身事件 */
 /* 用一个计时器记录未移动时间, flag记录鼠标是否在控制栏上。 */
 var controlPanelTimer = 0;
 var onControlPanelFlag = false;
 video.on('mousemove', function() {
     /* 如果鼠标在屏幕上移动，则显示控制条并重设计时器。 */
     controlPanel.show();
-    controlPanelTimer = 5000;
+    controlPanelTimer = 4000;
 });
 
 /* 若在控制栏上则为true，否则为false。 */
