eca674b237715a0e96af3f8b3671c2873af82eda
jajajag <jajajag@users.noreply.github.com>
2017-06-03 15:43:33 -0400

    Update jag-h5player.js

--- js/jag-h5player.js
+++ js/jag-h5player.js
@@ -36,9 +36,7 @@ var fullscreenItem = $('#fullscreen-item');
 function getFullscreen() {
     /* Check if the video is alrealy in fullscreen mode. */
     if (document.fullscreenElement || document.webkitFullscreenElement ||
-        document.mozFullScreenElement || document.msFullscreenElement || video[0].webkitDisplayingFullscreen) {
-        video[0].webkitExitFullscreen();
-        return;
+        document.mozFullScreenElement || document.msFullscreenElement || document.webkitDisplayingFullscreen) {
         /* Exit fullscreen if corresponding element exists. */
         if (document.exitFullscreen) {
             document.exitFullscreen();
@@ -48,9 +46,6 @@ function getFullscreen() {
             document.mozCancelFullScreen();
         } else if (document.msExitFullscreen) {
             document.msExitFullscreen();
-        } else if (video[0].webkitExitFullscreen) {
-            alert(222);
-            video[0].webkitExitFullscreen();
         }
     } else {
         /* Request fullscreen if corresponding element exists. */
